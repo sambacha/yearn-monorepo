c9897adaf6eed9c777dd68c5c5804a154ad41982
Luciano Bertenasco <lbertenasco@gmail.com>
2021-03-18 17:51:19 -0300

    feat: add keep3r votes governance functions

--- interfaces/keep3r/IKeep3rV1.sol
+++ interfaces/keep3r/IKeep3rV1.sol
@@ -12,6 +12,8 @@ interface IKeep3rV1 is IERC20 {
     function isBondedKeeper(address _keeper, address bond, uint256 _minBond, uint256 _earned, uint256 _age) external returns (bool);
     function addKPRCredit(address _job, uint256 _amount) external;
     function addJob(address _job) external;
+    function addVotes(address voter, uint256 amount) external;
+    function removeVotes(address voter, uint256 amount) external;
 
     function worked(address _keeper) external;
     function workReceipt(address _keeper, uint256 _amount) external;
